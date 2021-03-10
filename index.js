90b86ce95a6ae225986ab000944448fa0ca7ca65
Esteban Meza <meza.19@osu.edu>
2021-03-09 23:15:45 -0500

    initial setup of questions and gitignore

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-];
+// array of questions for user
+const questions = () => 
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the name of your project",
+        },
+        {
+            type: "input",
+            name: "description",
+            message : "Please describe the what, why and how of this project",
+        },
+        {
+            type:"input",
+            name: "installation",
+            message: "How do you install it"
+        },
+     
+
+        {
+            type: "input",
+            name: "usage",
+            message: "How do you use the app?",
+        },
+        {
+            type:"input",
+            name: "report",
+            message: "How do you report issues?",
+            
+        },
+         {
+             type:"input",
+             name: "contribution",
+             message: "How can contributions be made?",
+         },
+         {
+            type:"input",
+            name: "username",
+            message:"What is your GitHub username?",
+        },
+            {
+            type:"input",
+            name:"email",
+            message: "What is your email?",
+            }   
+
+    ]);
 
 // function to write README file
 function writeToFile(fileName, data) {
