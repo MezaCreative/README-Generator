b4ee538fd151f2b970dbb507ecaaa14584a73e89
Esteban Meza <meza.19@osu.edu>
2021-03-10 00:21:57 -0500

    Wrote framework for README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,45 @@
 function generateMarkdown(data) {
     return `# ${data.title}
   
+  ## Description
+
+  ${data.description}
+
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [Contributing](#contributing)
+
+  * [Issue Reportage](#Issue Reportage)
+
+  * [Questions](#questions)
+
+
+  ## Installation
+
+  To install ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contributing
+
+  Contribution can be made by ${data.contribution}
+
+  ## Issue Reportage
+  
+  Issues can be reported by ${data.report}
+
+  ## Questions
+
+  For any comments, questions, or concerns, please contact me directly at ${data.email}.
+  You can find my work here --> [${data.github}](https://github.com/${data.github}).
+  
   `;
   }
   
